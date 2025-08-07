500e6c81ba19b425631c0b3d6de533c6eb70a42e
Carmen Sorina Cosolan <carmensorina17@gmail.com>
2025-08-07 14:29:20 +0300

    QFP-108 Logout Automation

---
+++ QA Practice APP/Cosolan Sorina/Logout.spec.js
@@ -0,0 +1,15 @@
+import { test, expect,chromium } from '@playwright/test';
+
+test('test', async ({ }) => {
+const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://qa-practice.netlify.app/');
+  await page.getByRole('link', { name: 'Ecommerce - Login, Add to' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('admin@admin.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('link', { name: 'Log Out' }).click();
+});
