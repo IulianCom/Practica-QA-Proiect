81fb26fd4df791abc764f0d98f520c19b8c971d2
Amalia <amalia.dijmarescu@gmail.com>
2025-08-10 21:42:36 +0300

    Testeaza logarea cu email bun si parola gresita, email gresit si parola corecta si email gresit si parola gresita

---
+++ QA Practice APP/Dijmarescu Amalia/Invalid-login.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://qa-practice.netlify.app/tab');
+  await page.getByRole('link', { name: 'Ecommerce - Login, Add to' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('amalia@admin.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByText('Bad credentials! Please try').click();
+  await page.getByRole('link', { name: 'Ecommerce - Login, Add to' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('admin@admin.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('link', { name: 'Ecommerce - Login, Add to' }).click();
+  await page.getByRole('button', { name: 'Submit' }).click();
+});
